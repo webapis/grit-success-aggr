5a38e28f38459e4de607de25cf7ee97cb3243756
Serdar Ashirov <webapis@users.noreply.github.com>
2025-06-20 13:31:55 +0500

    Update getFilteredLinks.js

--- sites/products/helpers/getFilteredLinks.js
+++ sites/products/helpers/getFilteredLinks.js
@@ -2,7 +2,7 @@ export default async function getFilteredLinks(page, classNames, excludePatterns
   const hrefs = await page.evaluate((classNames, excludePatterns) => {
     return [...document.querySelectorAll("a")]
       .filter(a =>
-        classNames.some(cls => a.classList.contains(cls)) &&
+      //  classNames.some(cls => a.classList.contains(cls)) &&
         !excludePatterns.some(pattern => a.href.includes(pattern))
       )
       .map(a => a.href);
