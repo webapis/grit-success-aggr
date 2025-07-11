44b527f8af985ce3c88714915aca79553d7a9ab6
Serdar Ashirov <webapis@users.noreply.github.com>
2025-07-11 12:17:05 +0500

    Update priceSelector.js

--- sites/products/helpers/priceSelector.js
+++ sites/products/helpers/priceSelector.js
@@ -18,5 +18,7 @@ export default [
     '.price__previous_mobile.product-list-price',
     '.prices_mobile_custom .product-sale-price',
     '.mainFiyat span',
-    '.mainFiyat .fiyat'
+    '.mainFiyat .fiyat',
+    '.priceWrapper .currentPrice',
+    '.text-line.discountedPrice.pr'
 ]
