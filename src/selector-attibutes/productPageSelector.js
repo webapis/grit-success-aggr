e7563944e6f2d30f1d7b5b2402a488da052e0ed6
Serdar Ashirov <webapis@users.noreply.github.com>
2025-07-15 16:12:25 +0500

    morrez

--- src/selector-attibutes/productPageSelector.js
+++ src/selector-attibutes/productPageSelector.js
@@ -1,4 +1,5 @@
 export default [
+    '.category__list__main .infinite-scroll-component__outerdiv .infinite-scroll-component ',
     '#ProductPageProductList.ProductList',//elleshoes
     '.js-list-product.list__products',//koton
     '#product-container .catalog-products',//defacto
