1e37dd32aec62fb4d3cc75f421d15f135d555e9b
Serdar Ashirov <webapis@users.noreply.github.com>
2025-07-29 12:45:55 +0500

    Update prodCrawlerPuppeteer.js

--- prodCrawlerPuppeteer.js
+++ prodCrawlerPuppeteer.js
@@ -49,7 +49,7 @@ if (siteUrls.paused) {
     navigationTimeoutSecs: 120,
     headless: HEADLESS === "false" ? false : true,
     requestHandlerTimeoutSecs: 600000,
-    maxRequestsPerCrawl: 50
+   // maxRequestsPerCrawl: 50
   });
 
   crawler.run(siteUrls.urls);
