7f997f1e577c399e12b7c3d2566925eba11e87e2
Serdar Ashirov <webapis@users.noreply.github.com>
2025-08-14 14:22:46 +0500

    Update pushToGit.js

--- src/pushToGit.js
+++ src/pushToGit.js
@@ -21,8 +21,9 @@ const dataset = await Dataset.open(site);
 const datasetTotalItemsToBeCallected = await Dataset.open('totalItemsToBeCallected');
 const { items: data } = await dataset.getData();
 const { items: totalItemsToCallect } = await datasetTotalItemsToBeCallected.getData();
-const totalItemsPerPageDataset = await Dataset.open('totalItemsPerPage');
-const { items: totalItemsPerPage } = await totalItemsPerPageDataset.getData();
+const { items } = await totalItemsPerPageDataset.getData();
+
+const totalItemsPerPage = items?.[0]?.totalItemsPerPage
 const dataWithoutError = data.filter(f => !f.error);
 const dataWithError = data.filter(f => f.error);
 const { oldestTimestamp, newestTimestamp, minutesSpan } = getAggrTimeSpan({ data });
