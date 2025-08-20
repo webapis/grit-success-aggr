da1a69ab312f1e2f5034ab3f9433242b9a1f98d3
Serdar Ashirov <webapis@users.noreply.github.com>
2025-08-20 09:57:01 +0500

    Update productItemSelector.js

--- src/selector-attibutes/productItemSelector.js
+++ src/selector-attibutes/productItemSelector.js
@@ -1,4 +1,5 @@
 export default [
+    'ozd-custom-product-grid-item .carouselItem',
     '.grid__item.grid-product.medium-up--one-half',
     '[id*=product-tile-wrapper-]',
     'ozd-custom-product-grid-item',
