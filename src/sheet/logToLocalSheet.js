325efd81bae8b36ed374684b5385dd154a7aaf62
Serdar Ashirov <webapis@users.noreply.github.com>
2025-08-23 11:48:17 +0500

    save

--- src/sheet/logToLocalSheet.js
+++ src/sheet/logToLocalSheet.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-const logFilePath = path.resolve("logToSheet.json");
+const logFilePath = path.resolve("logToLocalSheet.json");
 
 export default function logToLocalSheet(newData) {
   let existingData = {};
