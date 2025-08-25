d2cd8711f50b544ca19d6ade0f1c2ca175b46408
Serdar Ashirov <webapis@users.noreply.github.com>
2025-08-25 10:02:21 +0500

    save

--- src/route/helper/continueIfProductPage.js
+++ src/route/helper/continueIfProductPage.js
@@ -1,13 +1,10 @@
 import dotenv from "dotenv";
 import productItemSelector from "../../selector-attibutes/productItemSelector.js";
 import findBestSelector from "../micro/findBestSelector.js";
-import { emitAsync } from "../../events.js";
 import { uploadImage } from "../../git/uploadImage.js";
-import '../../listeners.js'; // ← This registers event handlers
-import { pushDataToDataset } from "../../crawlee/datasetOperations.js";
 import getTotalItemsCount from "../micro/getTotalItemsCount.js";
 import logToLocalSheet from "../../sheet/logToLocalSheet.js";
-import baseRowData from "../micro/baseRowData.js";
+
 dotenv.config({ silent: true });
 const site = process.env.site;
 
@@ -19,14 +16,14 @@ export default async function continueIfProductPage({ page, siteUrls }) {
     });
 
 
-    const waitForSeconds = siteUrls?.waitForSeconds || 3;
+    //const waitForSeconds = siteUrls?.waitForSeconds || 3;
 
 
-    if (waitForSeconds > 0) {
-        await page.evaluate(async (seconds) => {
-            await new Promise(resolve => setTimeout(resolve, seconds * 1000)); // Fixed: multiply by 1000 for milliseconds
-        }, waitForSeconds);
-    }
+    // if (waitForSeconds > 0) {
+    //     await page.evaluate(async (seconds) => {
+    //         await new Promise(resolve => setTimeout(resolve, seconds * 1000)); // Fixed: multiply by 1000 for milliseconds
+    //     }, waitForSeconds);
+    // }
 
     const bestSelector = await findBestSelector(page, productItemSelector);
 
@@ -37,42 +34,31 @@ export default async function continueIfProductPage({ page, siteUrls }) {
         const { count: totalItemsToBeCallected, selector: totalItemsSelector } = await getTotalItemsCount(page, siteUrls?.totalProductCounterSelector);
 
 
-            logToLocalSheet({ totalItemsToBeCallected, totalItemsSelector });
-            //  await pushDataToDataset('totalItemsToBeCallected', { totalItemsToBeCallected, totalItemsSelector });
-        
+        logToLocalSheet({ totalItemsToBeCallected, totalItemsSelector });
+
+
 
         const totalItemsPerPage = bestSelector['count'];
-       // const matchedproductItemSelectors = [bestSelector['selector']]
 
-        //  await pushDataToDataset('totalItemsPerPage', { totalItemsPerPage });
-        //  await pushDataToDataset("matchedproductItemSelectors", { matchedproductItemSelectors });
         logToLocalSheet({ totalItemsPerPage });
         logToLocalSheet({ productItemSelector: bestSelector.selector });
 
-        return { success: true, productItemSelector: bestSelector.selector, totalItemsPerPage };
-
+        return true
 
     } else {
         //take screenshot if initial pages could not be retrieved.
         const screenshotBuffer = await page.screenshot({ fullPage: true });
-
-
         // Upload directly to GitHub
         const result = await uploadImage({
             fileName: `${site}-${Date.now()}.png`,  // Will become 'webpage-screenshot.png'
             imageBuffer: screenshotBuffer,   // Pass the buffer directly
             gitFolder: 'screenshots'
         })
-        console.log('Screenshot uploaded!')
-        console.log('View at:', result.url)
-        console.log('Direct link:', result.downloadUrl)
 
-        await emitAsync('log-to-sheet', {
-            sheetTitle: 'Crawl Logs(success)',
-            message: console.log(`Site ${site} is logging data to Google Sheet.`),
-            rowData: { ...baseRowData, Site: site, ScreenshotGit: result.url, Notes: `continueIfProductPage.js >`, productItemSelector: matchedproductItemSelectors.join(',') }
-        });
-        console.log('No product items found on the page');
-        return { success: false, productItemSelector: bestSelector.selector, totalItemsPerPage };
+        logToLocalSheet({ totalItemsPerPage });
+        logToLocalSheet({ productItemSelector: bestSelector.selector });
+        logToLocalSheet({ ScreenshotGit: result.url });
+
+        return false
     }
 }
