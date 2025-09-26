2c18771956bfdcd917936d9fe6e676d0b57f59ba
Serdar Ashirov <webapis@users.noreply.github.com>
2025-09-26 15:22:47 +0500

    Update pageUtilities.js

--- src/1_scraping/extraction/pageUtilities.js
+++ src/1_scraping/extraction/pageUtilities.js
@@ -649,7 +649,7 @@ function extractImageInfo(container, imageSelectors, imageAttributes,imgExtToFil
 
     // Define stop words for irrelevant URLs
     const imageStopWords = [
-         'placeholder',
+         'placeholder','load.gif',
         // 'loading', 'spinner', 'loader', 'blank', 'empty',
         // 'icon', 'logo', 'sprite', 'thumbnail', 'thumb', 'avatar',
         // 'banner', 'header', 'footer', 'background', 'bg',
