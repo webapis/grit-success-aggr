c5929c096fd0effd6f7ba9c20e73afc8ea0fc4d1
Serdar Ashirov <webapis@users.noreply.github.com>
2025-09-30 09:48:23 +0500

    Update pageUtilities.js

--- src/1_scraping/extraction/pageUtilities.js
+++ src/1_scraping/extraction/pageUtilities.js
@@ -649,7 +649,7 @@ function extractImageInfo(container, imageSelectors, imageAttributes,imgExtToFil
 
     // Define stop words for irrelevant URLs
     const imageStopWords = [
-         'placeholder','load.gif','hebebedundefined','loader.gif',
+         'placeholder','load.gif','hebebedundefined','loader.gif','https://mynameiso.co/wp-content/uploads/2025/03/trapeziosage-500x600.jpg',
         // 'loading', 'spinner', 'loader', 'blank', 'empty',
         // 'icon', 'logo', 'sprite', 'thumbnail', 'thumb', 'avatar',
         // 'banner', 'header', 'footer', 'background', 'bg',
