c04b57be4b5048cc3d1c6a1ab8b2c5c92163a4af
Serdar Ashirov <webapis@users.noreply.github.com>
2025-10-09 14:12:41 +0500

    Update continueIfProductPage.js

--- src/1_scraping/navigation/helper/continueIfProductPage.js
+++ src/1_scraping/navigation/helper/continueIfProductPage.js
@@ -16,7 +16,7 @@ export default async function continueIfProductPage({ page, siteUrls }) {
 
     await delay(5000); // wait for 5 seconds
     const bestSelector = await findBestSelector(page, productItemSelector);
-    console.log('bestSelector', bestSelector)
+
     debugger
     const { totalItemsToBeCallected, debug } = logToLocalSheet() || {};
     if (bestSelector.count > 0) {
