7b39bd6887a283e3f5f135f4d0fa89821ad4f049
Serdar Ashirov <webapis@users.noreply.github.com>
2025-10-10 10:19:49 +0500

    Update fistRoute.js

--- src/1_scraping/navigation/fistRoute.js
+++ src/1_scraping/navigation/fistRoute.js
@@ -13,7 +13,7 @@ dotenv.config({ silent: true });
 const site = process.env.site;
 
 export default async function first(props) {
-    const { page, addRequests, siteUrls, request: { url } } = props
+    const { page, addRequests, siteUrls, request: { url }, uploadScreenshot } = props
 
 
     console.log('inside first route')
@@ -36,11 +36,14 @@ debugger
         console.log('data',data.length)
         return data
     } else {
+        // This is where the selector failure is handled.
+        // We will now take a screenshot and log its URL.
+        const screenshotUrl = await uploadScreenshot(page, site);
         const { pageItems = [], pageNumbers = [] } = logToLocalSheet()
         const pageNumber = extractPageNumber(url, paginationParameterName) || 1
         const mergePageItems = [...pageItems, 0]
-        logToLocalSheet({ pageItems: mergePageItems, pageNumbers: [...pageNumbers, pageNumber] })
-       console.log('data2',0)
+        logToLocalSheet({ pageItems: mergePageItems, pageNumbers: [...pageNumbers, pageNumber], screenshotUrl: screenshotUrl || 'N/A' })
+        console.log('data2',0)
         return []
     }
 
