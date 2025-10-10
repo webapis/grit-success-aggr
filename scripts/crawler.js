0e9006021806d9f31285ef2310ed3b84af291e21
Serdar Ashirov <webapis@users.noreply.github.com>
2025-10-10 11:47:46 +0500

    Update crawler.js

--- scripts/crawler.js
+++ scripts/crawler.js
@@ -2,8 +2,8 @@ import { PuppeteerCrawler } from "crawlee";
 import fs from 'fs';
 import puppeteer from '../src/1_scraping/helpers/puppeteer-stealth.js';
 import preNavigationHooks from "./helpers/preNavigationHooksProd2.js";
-import { ForbiddenError, handleRequestFailure, handleForbiddenError } from '../src/2_data/processing/failure/failureHandler.js';
-import { summarizeAndReportRun } from '../src/2_data/processing/failure/runReporter.js';
+import { ForbiddenError, handleRequestFailure, handleForbiddenError } from './failureHandler.js';
+import { summarizeAndReportRun } from './runReporter.js';
 import { emitAsync } from '../src/shared/events.js';
 import logToLocalSheet from '../src/2_data/persistence/sheet/logToLocalSheet.js';
 
